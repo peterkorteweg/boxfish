81f06cff03fc6653cc5eb2e0535fcbdd020feca7
peterkorteweg <56510557+peterkorteweg@users.noreply.github.com>
2022-03-01 21:50:20 +0100

    Update __init__.py

--- boxfish/__init__.py
+++ boxfish/__init__.py
@@ -7,11 +7,6 @@ from boxfish.data import website
 from boxfish.data.website import get_website
 
 # Debugging
-from boxfish.utils import dicts
-from boxfish.utils import drivers
-from boxfish.utils import lists
-from boxfish.utils import times
-from boxfish.utils import urls
-from boxfish.utils import xpaths
+import boxfish.utils
 
 print('Initializing boxfish ...')
