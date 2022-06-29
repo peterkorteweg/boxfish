04fd47d39694c4b5db1e7444cfe7d47f18d4c95b
peterkorteweg <56510557+peterkorteweg@users.noreply.github.com>
2022-06-29 19:24:56 +0200

    Update __init__.py
    Remove references to current directory

--- boxfish/__init__.py
+++ boxfish/__init__.py
@@ -5,14 +5,14 @@
 import pathlib
 
 # Main modules
-from boxfish.data import config
-from boxfish.data import soups
-from boxfish.data import website
+from .data import config
+from .data import soups
+from .data import website
 
 # Main functions
-from boxfish.data.config import create
-from boxfish.data.config import build
-from boxfish.data.website import extract
+from .data.config import create
+from .data.config import build
+from .data.website import extract
 
 # Version
 __version__ = (pathlib.Path(__file__).parent / "VERSION").read_text()
