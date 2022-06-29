235f4385b1f71935a683869e5174d0f51e47f6ad
peterkorteweg <56510557+peterkorteweg@users.noreply.github.com>
2022-06-29 19:43:56 +0200

    Update __init__.py

--- boxfish/__init__.py
+++ boxfish/__init__.py
@@ -4,15 +4,16 @@
 
 import pathlib
 
+from . import data
+
 # Main modules
-from .data import config
-from .data import soups
-from .data import website
+from data import config
+from data import soups
+from data import website
 
 # Main functions
-from .data.config import create
-from .data.config import build
-from .data.website import extract
+from data.config import build, create
+from data.website import extract
 
 # Version
 __version__ = (pathlib.Path(__file__).parent / "VERSION").read_text()
